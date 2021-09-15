31fa3c188c247fc2fe41d89b677f62210065c1a6
Aravind Jaimon <email@aravindjaimon.me>
2021-09-15 17:39:47 +0530

    added sidebar

---
+++ src/Components/Teams/Sidebar.tsx
@@ -0,0 +1,50 @@
+import { ActionButton, IButtonStyles, Stack } from "@fluentui/react";
+import React from "react";
+
+const Sidebar = () => {
+  const buttonStyle: IButtonStyles = {
+    icon: { color: "white", fontSize: 20 },
+    textContainer: { width: "100%", color: "white" },
+  };
+  const actions = [
+    { iconName: "ActivityFeed", name: "Activity" },
+    { iconName: "OfficeChat", name: "Chat" },
+    { iconName: "TeamsLogo", name: "Team" },
+    { iconName: "Calendar", name: "Meetings" },
+    { iconName: "Phone", name: "Calls" },
+    { iconName: "More" },
+  ];
+  return (
+    <Stack
+      tokens={{ childrenGap: 20 }}
+      horizontalAlign="center"
+      verticalAlign="start"
+      styles={{
+        root: {
+          paddingTop: 10,
+          backgroundColor: "#33344A",
+          width: 75,
+          height: "96.3vh",
+          color: "White",
+          fontSize: "0.5rem",
+        },
+      }}
+    >
+      {actions.map((action, index) => (
+        <Stack.Item key={index}>
+          <Stack horizontalAlign="center" verticalAlign="center">
+            <Stack.Item>
+              <ActionButton
+                styles={buttonStyle}
+                iconProps={{ iconName: action.iconName }}
+              />
+            </Stack.Item>
+            <Stack.Item>{action.name}</Stack.Item>
+          </Stack>
+        </Stack.Item>
+      ))}
+    </Stack>
+  );
+};
+
+export default Sidebar;
--- src/Components/Teams/Teams.tsx
+++ src/Components/Teams/Teams.tsx
@@ -1,12 +1,20 @@
 import { Stack } from "@fluentui/react";
 import React from "react";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const Teams = () => {
   return (
     <Stack>
       <Stack.Item>
         <Header />
+      </Stack.Item>
+      <Stack.Item>
+        <Stack horizontal>
+          <Stack.Item>
+            <Sidebar />
+          </Stack.Item>
+        </Stack>
       </Stack.Item>
     </Stack>
   );
